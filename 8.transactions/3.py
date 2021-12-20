76f6877209c5aac15867dada4daa4917e854a650
Albert Gutowski <ablikexe@gmail.com>
2021-12-20 13:01:57 +0100

    add DELETE example

--- 8.transactions/3.py
+++ 8.transactions/3.py
@@ -3,8 +3,12 @@ from transactions import prepare_db, begin_transaction
 
 prepare_db()
 
+t0 = begin_transaction('t0', isolation_level=1)
+t0.insert(1)
+t0.commit()
+
 t1 = begin_transaction('t1', isolation_level=1)
-t1.insert(1)
+t1.delete_all(1)
 t1.select()
 t1.rollback()
 
